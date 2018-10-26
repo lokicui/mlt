3fc13ad801e4b691ebe2dcb447b0615dba898a2d
loki <loki@bjsjs_11_83.(none)>
2018-10-26 15:41:47 +0800

    modify es address

--- g/const.go
+++ g/const.go
@@ -12,4 +12,6 @@ var Addr = flag.String("addr", "0.0.0.0", "Specify local addr for remote connect
 var Port = flag.Int("port", 8080, "Specify local port for remote connects")
 var Debug = flag.Int("debug", 0, "debug level, 1 for debug")
 var Version = flag.Bool("v", false, "show version")
-var ESAddrs = flag.String("esAddrs", "http://10.134.13.99:9200,http://10.134.14.27:9200,http://10.134.28.85:9200", "Specify ES clients for search")
+
+//var ESAddrs = flag.String("esAddrs", "http://10.134.13.99:9200,http://10.134.14.27:9200,http://10.134.28.85:9200", "Specify ES clients for search")
+var ESAddrs = flag.String("esAddrs", "http://rsync.master01.luedonges.sjs.ted:9200,http://rsync.master02.luedonges.sjs.ted:9200,http://rsync.master03.luedonges.sjs.ted:9200", "Specify ES clients for search")
